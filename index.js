2db061dd2c27f532d4f4b4b5a68926790a8764b3
SurfingBeast <158038190+SurfingBeast@users.noreply.github.com>
2024-01-29 20:48:46 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
+const { Client } = require('discord.js');
+const client = new Client();
 
+// Require your discord.js file
+require('./discord');
+
+// Optionally, you can add more code here if needed
+
+// Login to the Discord bot
+client.login('your-bot-token');
